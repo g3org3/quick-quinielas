6174258d0bc2988c8a6f85377e9cbf00ada33379
george <7jagjag@gmail.com>
2024-06-20 16:41:43 +0200

    update

--- src/pocketbase-types.ts
+++ src/pocketbase-types.ts
@@ -3,13 +3,15 @@
 */
 
 import type PocketBase from 'pocketbase'
-import type { RecordService, RecordFullListOptions, RecordListOptions } from 'pocketbase'
 import { z } from 'zod'
+import type { RecordService, RecordFullListOptions, RecordListOptions } from 'pocketbase'
 
 export enum Collections {
+  Leaderboard = "leaderboard",
   Links = "links",
   Matches = "matches",
   Predictions = "predictions",
+  Results = "results",
   Snapshots = "snapshots",
   Sprints = "sprints",
   Tickets = "tickets",
@@ -41,6 +43,11 @@ export type AuthSystemFields<T = never> = {
 
 // Record types for each collection
 
+export type LeaderboardRecord<Tpoints = unknown> = {
+  points?: null | Tpoints
+  user?: RecordIdString
+}
+
 export type LinksRecord<Ttags = unknown> = {
   author?: RecordIdString
   desc?: string
@@ -69,6 +76,18 @@ export type PredictionsRecord = {
   user?: RecordIdString
 }
 
+export type ResultsRecord<Tpoints = unknown> = {
+  away?: string
+  awayScore?: number
+  home?: string
+  homeScore?: number
+  match_id?: RecordIdString
+  p_away?: number
+  p_home?: number
+  points?: null | Tpoints
+  user?: RecordIdString
+}
+
 export type SnapshotsRecord = {
   fetchedat?: IsoDateString
   sprint?: RecordIdString
@@ -91,18 +110,22 @@ export type TicketsRecord<Tlabels = unknown, Tparents = unknown> = {
 }
 
 export type TournamentsRecord = {
+  logo?: string
   name?: string
 }
 
 export type UsersRecord = {
   avatar?: string
+  img?: string
   name?: string
 }
 
 // Response types include system fields and match responses from the PocketBase API
+export type LeaderboardResponse<Tpoints = unknown, Texpand = unknown> = Required<LeaderboardRecord<Tpoints>> & BaseSystemFields<Texpand>
 export type LinksResponse<Ttags = unknown, Texpand = unknown> = Required<LinksRecord<Ttags>> & BaseSystemFields<Texpand>
 export type MatchesResponse<Texpand = unknown> = Required<MatchesRecord> & BaseSystemFields<Texpand>
 export type PredictionsResponse<Texpand = unknown> = Required<PredictionsRecord> & BaseSystemFields<Texpand>
+export type ResultsResponse<Tpoints = unknown, Texpand = unknown> = Required<ResultsRecord<Tpoints>> & BaseSystemFields<Texpand>
 export type SnapshotsResponse<Texpand = unknown> = Required<SnapshotsRecord> & BaseSystemFields<Texpand>
 export type SprintsResponse<Texpand = unknown> = Required<SprintsRecord> & BaseSystemFields<Texpand>
 export type TicketsResponse<Tlabels = unknown, Tparents = unknown, Texpand = unknown> = Required<TicketsRecord<Tlabels, Tparents>> & BaseSystemFields<Texpand>
@@ -112,9 +135,11 @@ export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSyste
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
+  leaderboard: LeaderboardRecord
   links: LinksRecord
   matches: MatchesRecord
   predictions: PredictionsRecord
+  results: ResultsRecord
   snapshots: SnapshotsRecord
   sprints: SprintsRecord
   tickets: TicketsRecord
@@ -123,9 +148,11 @@ export type CollectionRecords = {
 }
 
 export type CollectionResponses = {
+  leaderboard: LeaderboardResponse
   links: LinksResponse
   matches: MatchesResponse
   predictions: PredictionsResponse
+  results: ResultsResponse
   snapshots: SnapshotsResponse
   sprints: SprintsResponse
   tickets: TicketsResponse
@@ -137,9 +164,11 @@ export type CollectionResponses = {
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
+  collection(idOrName: 'leaderboard'): RecordService<LeaderboardResponse>
   collection(idOrName: 'links'): RecordService<LinksResponse>
   collection(idOrName: 'matches'): RecordService<MatchesResponse>
   collection(idOrName: 'predictions'): RecordService<PredictionsResponse>
+  collection(idOrName: 'results'): RecordService<ResultsResponse>
   collection(idOrName: 'snapshots'): RecordService<SnapshotsResponse>
   collection(idOrName: 'sprints'): RecordService<SprintsResponse>
   collection(idOrName: 'tickets'): RecordService<TicketsResponse>
@@ -149,6 +178,12 @@ export type TypedPocketBase = PocketBase & {
 
 
 // Generated Schemas
+
+export const leaderboardSchema = z.object({
+  user: z.string().nullish(),
+  points: z.number().nullish(),
+})
+
 export const matchesSchema = z.object({
   home: z.string().nullish(),
   away: z.string().nullish(),
@@ -168,18 +203,44 @@ export const predictionsSchema = z.object({
   match: z.string().nullish(),
 })
 
+export const resultsSchema = z.object({
+  user: z.string().nullish(),
+  match_id: z.string().nullish(),
+  p_home: z.number().nullish(),
+  p_away: z.number().nullish(),
+  home: z.string().nullish(),
+  homeScore: z.number().nullish(),
+  away: z.string().nullish(),
+  awayScore: z.number().nullish(),
+  points: z.number().nullish(),
+})
+
+export const sprintsSchema = z.object({
+  name: z.string().nullish(),
+})
+
 export const tournamentsSchema = z.object({
   name: z.string().nullish(),
+  logo: z.string().url().nullish(),
 })
 
 export const usersSchema = z.object({
   name: z.string().nullish(),
   avatar: z.string().nullish(),
+  img: z.string().url().nullish(),
 })
 
 
 
 // Generated Hooks
+
+export async function useGetAllLeaderboard(pb: PocketBase, options: RecordFullListOptions) {
+  return await pb.collection(Collections.Leaderboard).getFullList<LeaderboardResponse>(options)
+}
+
+export async function useGetLeaderboard(pb: PocketBase, page: number, perPage: number, options: RecordListOptions) {
+  return await pb.collection(Collections.Leaderboard).getList<LeaderboardResponse>(page, perPage, options)
+}
 
 export async function useGetAllLinks(pb: PocketBase, options: RecordFullListOptions) {
   return await pb.collection(Collections.Links).getFullList<LinksResponse>(options)
@@ -203,6 +264,14 @@ export async function useGetAllPredictions(pb: PocketBase, options: RecordFullLi
 
 export async function useGetPredictions(pb: PocketBase, page: number, perPage: number, options: RecordListOptions) {
   return await pb.collection(Collections.Predictions).getList<PredictionsResponse>(page, perPage, options)
+}
+
+export async function useGetAllResults(pb: PocketBase, options: RecordFullListOptions) {
+  return await pb.collection(Collections.Results).getFullList<ResultsResponse>(options)
+}
+
+export async function useGetResults(pb: PocketBase, page: number, perPage: number, options: RecordListOptions) {
+  return await pb.collection(Collections.Results).getList<ResultsResponse>(page, perPage, options)
 }
 
 export async function useGetAllSnapshots(pb: PocketBase, options: RecordFullListOptions) {
