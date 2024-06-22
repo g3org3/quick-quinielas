aee60675914e3ee9721cbbc5d45e93df180491e1
george <7jagjag@gmail.com>
2024-06-22 22:57:49 +0200

    login via email

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,11 @@
-import { Flex, Button } from "@chakra-ui/react";
+import { Flex, Button, Input } from "@chakra-ui/react";
 import type { ClientResponseError } from "pocketbase";
 import { pb } from "../pb";
+import { useState } from "react";
 
 export default function Login() {
+  const [state, setState] = useState('')
+
   const onLogin = async () => {
     let res = null;
     try {
@@ -18,22 +21,59 @@ export default function Login() {
     document.location = "/";
   };
 
+  const onEmailLogin: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(e.currentTarget)
+    const form: Record<string, string> = {}
+    for (const [key, value] of data.entries()) {
+      form[key] = String(value)
+    }
+
+    try {
+      await pb.collection("users").authWithPassword(form.username, form.password);
+    } catch (e) {
+      const err = e as ClientResponseError;
+      alert(err.message);
+    }
+
+    document.location = "/";
+  }
+
   return (
-    <Flex h="100dvh" alignItems="center" justifyContent="center" bg="gray.100">
-      <Flex
-        p="6"
-        w="300px"
-        bg="gray.50"
-        boxShadow="md"
-        border="1px solid"
-        borderColor="gray.100"
-        flexDir="column"
-        color="black"
-        gap="4"
-      >
-        <Flex fontSize="xx-large">Quiniela | Login</Flex>
-        <Button colorScheme="blue" onClick={onLogin}>login with google</Button>
+    <form onSubmit={onEmailLogin}>
+      <Flex h="100dvh" alignItems="center" justifyContent="center" bg="gray.100">
+        <Flex
+          p="6"
+          w="300px"
+          bg="gray.50"
+          boxShadow="md"
+          border="1px solid"
+          borderColor="gray.100"
+          flexDir="column"
+          color="black"
+          gap="4"
+        >
+          <Flex fontSize="xx-large">Quiniela | Login</Flex>
+          {state === 'email'
+            ? null
+            : <Button colorScheme="blue" onClick={onLogin}>login with google</Button>
+          }
+          {state === 'email'
+            ? <>
+              <Input name="username" placeholder="email" />
+              <Input name="password" placeholder="password" type="password" />
+            </>
+            : null
+          }
+          {state === 'email'
+            ? null
+            : <Button colorScheme="gray" onClick={() => setState('email')}>login with email</Button>
+          }
+          {state === 'email' ? <Button type="submit">Login</Button> : null}
+          {state === 'email' ? <Button onClick={() => setState('')}>Home</Button> : null}
+        </Flex>
       </Flex>
-    </Flex>
+    </form>
   );
 }
