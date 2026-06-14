3000d9c57e2bca017aeb7336ae729c4936928b9e
george <7jagjag@gmail.com>
2026-06-14 19:35:48 +0200

    fix bottom nav added perfil

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,21 +1,49 @@
-import { Link } from '@tanstack/react-router'
-import { Flex, Button } from '@chakra-ui/react'
+import { Link } from "@tanstack/react-router";
+import { Flex, Button } from "@chakra-ui/react";
+import { pb } from "@/pb";
+import { UsersResponse } from "@/pocketbase-types";
 
-export default function BottomNav({ tournamentId, state }: { tournamentId: string, state?: 'vaticinios' | 'puntos' }) {
+export default function BottomNav({
+  tournamentId,
+  state,
+}: {
+  tournamentId: string;
+  state?: "vaticinios" | "puntos" | "perfil";
+}) {
+  const userId = (pb.authStore.model as UsersResponse).id;
+
   return (
     <>
       <hr />
       <Flex alignItems="center" gap="2" mb="3">
-        <Link style={{ width: '100%' }} to="/">
-          <Button w="100%" variant="ghost">Torneos</Button>
+        <Link
+          style={{ width: "100%" }}
+          to="/tournaments/$tournamentId/$userId"
+          params={{ tournamentId, userId }}
+        >
+          <Button isActive={state === "perfil"} w="100%" variant="ghost">
+            Perfil
+          </Button>
         </Link>
-        <Link style={{ width: '100%' }} to="/tournaments/$tournamentId" params={{ tournamentId }}>
-          <Button isActive={state === 'vaticinios'} w="100%" variant="ghost">Vaticinios</Button>
+        <Link
+          style={{ width: "100%" }}
+          to="/tournaments/$tournamentId"
+          params={{ tournamentId }}
+        >
+          <Button isActive={state === "vaticinios"} w="100%" variant="ghost">
+            Vaticinios
+          </Button>
         </Link>
-        <Link style={{ width: '100%' }} to="/tournaments/$tournamentId/points" params={{ tournamentId }}>
-          <Button isActive={state === 'puntos'} w="100%" variant="ghost">Puntos</Button>
+        <Link
+          style={{ width: "100%" }}
+          to="/tournaments/$tournamentId/points"
+          params={{ tournamentId }}
+        >
+          <Button isActive={state === "puntos"} w="100%" variant="ghost">
+            Puntos
+          </Button>
         </Link>
       </Flex>
     </>
-  )
+  );
 }
